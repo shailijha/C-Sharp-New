024636233395c76102cd2a57f33b2a7e4ce63e9e
shailijha <shaili_jha@yahoo.co.in>
2019-09-14 12:06:31 -0400

    Removed Person class

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Classes
 {
-    public class Person
-    {
-        public string firstName;
-        public string lastName;
-
-        public void Introduce()
-        {
-            Console.WriteLine("My name is " + firstName + " " + lastName);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
