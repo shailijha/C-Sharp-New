60d644ecb75572070333b5e75158cf8482f9e975
shailijha <shaili_jha@yahoo.co.in>
2019-09-13 21:33:06 -0400

    Classes program with Person Class

---
+++ Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + firstName + " " + lastName);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person shaili = new Person();
+            shaili.firstName = "Shaili";
+            shaili.lastName = "Jha";
+            shaili.Introduce();
+        }
+    }
+}
