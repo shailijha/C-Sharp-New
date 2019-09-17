4051e89644f077fb57bb52a2fb4cddac257ca53e
shailijha <shaili_jha@yahoo.co.in>
2019-09-17 11:57:01 -0400

    Text Demo Class

---
+++ Text/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var fullName = "Shaili Jha ";
+            Console.WriteLine("Trim: '{0}'",fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.ToUpper().Trim());
+
+            Console.WriteLine(fullName.Split(' '));*/
+
+            var sentence = "This is going to be a really really really really really long text";
+            Console.WriteLine(StringUtility.SummarizeText(sentence, 25));
+        }
+    }
+}
