39cf82d4d184379da547096bc3ce0f3dee66c1e2
shailijha <shaili_jha@yahoo.co.in>
2019-09-17 12:30:05 -0400

    StringBuilder Demo

--- Text/Program.cs
+++ Text/Program.cs
@@ -16,8 +16,32 @@ namespace Text
 
             Console.WriteLine(fullName.Split(' '));*/
 
-            var sentence = "This is going to be a really really really really really long text";
-            Console.WriteLine(StringUtility.SummarizeText(sentence, 25));
+            //var sentence = "This is going to be a really really really really really long text";
+            //Console.WriteLine(StringUtility.SummarizeText(sentence, 25));
+
+            var builder = new StringBuilder("Hi there! How are you?");
+            /*builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
+            
+            builder.Replace('-', '=');
+            builder.Remove(0, 10);
+            builder.Insert(0, new string('-', 10));*/
+
+            builder.Append('-', 10)
+                   .AppendLine()
+                   .Append("Header")
+                   .AppendLine()
+                   .Append('-', 10)
+                   .Replace('-', '=')
+                   .Remove(0, 10)
+                   .Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First char: "+builder[0]);
         }
     }
 }
