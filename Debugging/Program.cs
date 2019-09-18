217a35866b545158e9c3c99875e94396c5072acc
shailijha <shaili_jha@yahoo.co.in>
2019-09-18 01:10:00 -0400

    Debugging Demo

--- Debugging/Program.cs
+++ Debugging/Program.cs
@@ -19,13 +19,19 @@ namespace Debugging
 
         public static List<int> GetSmallests(List<int> list, int count)
         {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            if (count > list.Count || count <= 0)
+                throw new ArgumentOutOfRangeException("count", "Count should be between 1 and the number of elements in the list");
+
+            var buffer = new List<int>(list);
             var smallests = new List<int>();
 
             while (smallests.Count < count)
             {
-                var min = GetSmallest(list);
+                var min = GetSmallest(buffer);
                 smallests.Add(min);
-                list.Remove(min);
+                buffer.Remove(min);
             }
 
             return smallests;
