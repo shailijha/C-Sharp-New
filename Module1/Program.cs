40e96376d68d3cec365c29e2dbf27a5c112880f1
shailijha <shaili_jha@yahoo.co.in>
2019-09-18 12:18:26 -0400

    Main program for module 1
    Classes and encapsulation implemented

---
+++ Module1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrinksMachine myMachine = new DrinksMachine("Hyderabad, India", "DM1000", "Brand");
+            
+            Console.WriteLine("myMachine location: {0}, myMachine make: {1}, myMachine model: {2}", myMachine.Location, myMachine.Make, myMachine.Model);
+            myMachine.MakeCappuccino();
+        }
+    }
+}
