0fd3fbf73731df7d71546b0436394366211da342
shailijha <shaili_jha@yahoo.co.in>
2019-09-18 21:58:54 -0400

    Car class

---
+++ Mod1_Lab1/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_Lab1
+{
+    public class Car
+    {
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public float Mileage { get; set; }
+    }
+}
