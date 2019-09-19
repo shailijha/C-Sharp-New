5a668ceb0ce4921277157814d41faff794089c29
shailijha <shaili_jha@yahoo.co.in>
2019-09-18 21:59:59 -0400

    Module1 Lab1 program edX C#

---
+++ Mod1_Lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new Car();
+            car1.Color = "white";
+            car1.Year = 2010;
+            car1.Mileage = 11000;
+
+            Console.WriteLine("Car 1's properties: {0} car built in {1} with {2} miles on it", car1.Color, car1.Year, car1.Mileage);
+            Console.WriteLine($"This car is painted {car1.Color}, was built in {car1.Year}, and has {car1.Mileage} miles on it");
+        }
+    }
+}
