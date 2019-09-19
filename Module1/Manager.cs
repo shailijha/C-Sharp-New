14c1166516414238aff12978433a0371bfa7c3fe
shailijha <shaili_jha@yahoo.co.in>
2019-09-19 12:33:14 -0400

    Added abstract class demo

---
+++ Module1/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    class Manager: Employee
+    {
+        private string department;
+
+        public string Department { get => department; set => department = value; }
+
+        public override void Login()
+        {
+            base.Login();
+            Console.WriteLine("Manager Login");
+        }
+
+        public override void Hire()
+        {
+            Console.WriteLine("Hire people");
+            //throw new NotImplementedException();
+        }
+    }
+}
