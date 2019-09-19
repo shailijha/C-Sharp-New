883f93a9988f8df56372e8b15ef52d96f1a74cc9
shailijha <shaili_jha@yahoo.co.in>
2019-09-18 22:01:40 -0400

    Module1 Lab2 Program edX C#

---
+++ Mod1_Lab2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new Car();
+            car1.Color = "white";
+            car1.Year = 2010;
+            car1.Mileage = 11000;
+
+            var car2 = new Car("Red", 2008);
+
+            Console.WriteLine($"There are {Car.CountCars()} cars on inventory right now");
+        }
+    }
+}
