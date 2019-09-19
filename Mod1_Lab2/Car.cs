451ddfd2a1eb5375f515e1fa2520010d2978e7bf
shailijha <shaili_jha@yahoo.co.in>
2019-09-18 22:00:43 -0400

    Car class for Module 1 Lab 2 edX C#

---
+++ Mod1_Lab2/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_Lab2
+{
+    public class Car
+    {
+        public Car(string color, int year)
+        {
+            this.Color = color;
+            this.Year = year;
+            instances++;
+        }
+
+        public Car(int year, float mileage)
+        {
+            this.Year = year;
+            this.Mileage = mileage;
+            instances++;
+        }
+
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public float Mileage { get; set; }
+
+        private static int instances = 0;
+
+        public Car()
+        {
+            instances++;
+        }
+
+        public static int CountCars()
+        {
+            return instances;
+        }
+    }
+}
