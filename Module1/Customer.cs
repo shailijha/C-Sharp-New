e6b2fbab28eed613decd2d868b66ca7803d66f33
shailijha <shaili_jha@yahoo.co.in>
2019-09-22 10:35:28 -0400

    Customer class

---
+++ Module1/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    class Customer: ILoyaltyCardHolder
+    {
+        private int totalPoints;
+
+        public int TotalPoints
+        {
+            get { return totalPoints;  }
+        }
+
+        public int AddPoints(decimal transactionValue)
+        {
+            int points = Decimal.ToInt32(transactionValue);
+            totalPoints += points;
+            return totalPoints;
+        }
+
+        public void ResetPoints()
+        {
+            totalPoints = 0;
+        }
+    }
+}
