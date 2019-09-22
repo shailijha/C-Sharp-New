46ef7d1a117481dd1bbe9e3d4e6f21f05484f369
shailijha <shaili_jha@yahoo.co.in>
2019-09-22 10:35:39 -0400

    Loyalty card holder interface

---
+++ Module1/ILoyaltyCardHolder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    interface ILoyaltyCardHolder
+    {
+        int TotalPoints { get; }
+        int AddPoints(decimal transactionValue);
+        void ResetPoints();
+    }
+}
