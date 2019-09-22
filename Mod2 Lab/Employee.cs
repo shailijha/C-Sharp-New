c8d530645bd4ff6330379efdb20793917f1285e3
shailijha <shaili_jha@yahoo.co.in>
2019-09-22 10:34:23 -0400

    Employee Class Mod2 Lab

---
+++ Mod2 Lab/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab
+{
+    class Employee
+    {
+        private string employeeName;
+        private double employeeBaseSalary;
+        private int employeeId;
+
+        public string EmployeeName { get => employeeName; set => employeeName = value; }
+        public double EmployeeBaseSalary { get => employeeBaseSalary; set => employeeBaseSalary = value; }
+        public int EmployeeId { get => employeeId; set => employeeId = value; }
+
+        public Employee(string name, double baseSalary)
+        {
+            this.EmployeeName = name;
+            this.EmployeeBaseSalary = baseSalary;
+        }
+
+        public string toString()
+        {
+            return this.EmployeeId + this.EmployeeName;
+        }
+    }
+}
