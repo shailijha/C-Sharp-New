fdaabdd7cf7b8cfa41b738ae7648891b6d76e02b
shailijha <shaili_jha@yahoo.co.in>
2019-09-23 17:51:33 -0400

    Created customer interface

---
+++ Practice/Customer.cs
@@ -0,0 +1,24 @@
+﻿namespace Practice
+{
+    public class Customer
+    {
+        public int id;
+        public string name;
+
+        public Customer()
+        {
+
+        }
+
+        public Customer(int id)
+        {
+            this.id = id;
+        }
+
+        public Customer(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
