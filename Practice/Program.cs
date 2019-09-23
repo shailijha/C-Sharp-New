e0fbf5262ab6c6e77cc2bcb7d36d6880edc69ee0
shailijha <shaili_jha@yahoo.co.in>
2019-09-23 17:52:09 -0400

    Created objects for constructor

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Use Ctrl + tab to switch between windows
+
 namespace Practice
 {
     public class Person
@@ -28,15 +30,18 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            Person p = new Person();
-            var p1 = new Person();
+            //Person p = new Person();
+            //var p1 = new Person();
 
-            var p3 = Person.Parse("Jha");
+            //var p3 = Person.Parse("Jha");
 
-            p.name = "Shaili";
-            p.Introduce("Mosh");
+            //p.name = "Shaili";
+            //p.Introduce("Mosh");
 
-            p3.Introduce("Mosh");
+            //p3.Introduce("Mosh");
+
+            var customer = new Customer(1, "John");
+            Console.WriteLine($"{customer.id} {customer.name}");
         }
     }
 }
