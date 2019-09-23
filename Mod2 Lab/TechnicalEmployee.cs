ccc414c9d045a2ab8fbe94c647ac3c74d0c1880a
shailijha <shaili_jha@yahoo.co.in>
2019-09-22 23:18:14 -0400

    Added props and methods

--- Mod2 Lab/TechnicalEmployee.cs
+++ Mod2 Lab/TechnicalEmployee.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Mod2_Lab
 {
-    class TechnicalEmployee
+    class TechnicalEmployee: Employee
     {
+        public int successfulCheckIns = 5;
+
+        public TechnicalEmployee(string name) : base(name, 75000)
+        {
+            
+        }
+
+        public override string employeeStatus()
+        {
+            return ($"{this.toString()} has {this.successfulCheckIns} successful check ins");
+            //return base.employeeStatus();
+        }
     }
 }
