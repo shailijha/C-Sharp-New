dafd730088668cf34e2a730797fc35dc2c8a1fc2
shailijha <shaili_jha@yahoo.co.in>
2019-09-23 15:40:54 -0400

    Main program class for Practice
    Classes demo

---
+++ Practice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Person
+    {
+        public string name;
+
+        public void Introduce(string name)
+        {
+            Console.WriteLine($"Hi there, {name}! I am {this.name}");
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.name = str;
+
+            return person;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            var p1 = new Person();
+
+            var p3 = Person.Parse("Jha");
+
+            p.name = "Shaili";
+            p.Introduce("Mosh");
+
+            p3.Introduce("Mosh");
+        }
+    }
+}
