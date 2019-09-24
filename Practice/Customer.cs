69efcb0b48beabda96a8312349147fdfbc4604a7
shailijha <shaili_jha@yahoo.co.in>
2019-09-24 00:45:55 -0400

    Added constructors

--- Practice/Customer.cs
+++ Practice/Customer.cs
@@ -1,23 +1,25 @@
-﻿namespace Practice
+﻿using System.Collections.Generic;
+
+namespace Practice
 {
     public class Customer
     {
         public int id;
         public string name;
+        public List<Order> Orders;
 
         public Customer()
         {
-
+            Orders = new List<Order>();
         }
 
-        public Customer(int id)
+        public Customer(int id): this()
         {
             this.id = id;
         }
 
-        public Customer(int id, string name)
+        public Customer(int id, string name): this(id)
         {
-            this.id = id;
             this.name = name;
         }
     }
