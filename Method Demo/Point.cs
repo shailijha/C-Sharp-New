38e1232763f0e3addebd3541a1352372ad9e6484
shailijha <shaili_jha@yahoo.co.in>
2019-09-24 21:11:53 -0400

    Point class to create a point object

---
+++ Method Demo/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Demo
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Move(Point newLocn)
+        {
+            if (newLocn == null)
+                throw new ArgumentNullException("newLocn");
+
+            Move(newLocn.x, newLocn.y);
+        }
+    }
+}
