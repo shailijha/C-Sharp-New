955ec175842a2f363c92f3ba5fcb688502738a41
shailijha <shaili_jha@yahoo.co.in>
2019-09-24 21:11:01 -0400

    Calculator class for params demo

---
+++ Method Demo/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Demo
+{
+    public class Calculator
+    {
+        public int Add(params int[] numbers)
+        {
+            var sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            return sum;
+        }
+    }
+}
