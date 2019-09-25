0d8b7c8f00c33039a8bf02aebc5a2f898e962002
shailijha <shaili_jha@yahoo.co.in>
2019-09-24 21:12:03 -0400

    Main program class

---
+++ Method Demo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            var result = int.TryParse("abc", out number);
+            if(result)
+                Console.WriteLine(result);
+            else
+                Console.WriteLine("Not converted");
+        }
+
+        static void UseParams()
+        {
+            var calc = new Calculator();
+            Console.WriteLine(calc.Add(2, -3, 9, -100));
+            Console.WriteLine(calc.Add(2, -3, 9));
+            Console.WriteLine(calc.Add(new int[] { 2, -3, 9, -100 }));
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine($"The point is at ({point.x}, {point.y})");
+
+                point.Move(30, 50);
+                Console.WriteLine($"The point is at ({point.x}, {point.y})");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured");
+            }
+        }
+    }
+}
