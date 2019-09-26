ed5fb33f76d8061e7ffedcb53eefa41e0865c3aa
shailijha <shaili_jha@yahoo.co.in>
2019-09-26 11:15:01 -0400

    Person class method demo

---
+++ Method Demo/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Demo
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthDate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthDate()
+        {
+            return _birthdate;
+        }
+    }
+}
