330a34a3f8805f86bde79af9c2d8e8ee4bf33e69
shailijha <shaili_jha@yahoo.co.in>
2019-10-01 10:48:44 -0400

    order processor class

---
+++ Interface/OrderProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public class OrderProcessor
+    {
+        private readonly ShippingCalculator _shippingCalculator;
+
+        public OrderProcessor()
+        {
+            _shippingCalculator = new ShippingCalculator();
+        }
+
+        public void Process(Order order)
+        {
+            if (order.IsShipped)
+                throw new InvalidOperationException("This order is already processed");
+
+            order.Shipment = new Shipment
+            {
+                Cost = _shippingCalculator.CalculateShipping(order),
+                ShippingDate = DateTime.Today.AddDays(1)
+            };
+        }
+    }
+}
