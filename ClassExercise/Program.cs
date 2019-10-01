85d088b8ff677fd2e4da0a4f18bb11c14b29cf93
shailijha <shaili_jha@yahoo.co.in>
2019-10-01 10:44:38 -0400

    Main program class stopwatch

--- ClassExercise/Program.cs
+++ ClassExercise/Program.cs
@@ -8,13 +8,49 @@ namespace ClassExercise
 {
     public class Stopwatch
     {
+        private DateTime startTime;
+        private DateTime endTime;
+        //private TimeSpan startTime = new TimeSpan(0,0,0);
+        //private TimeSpan endTime;
+        //private bool stopFlag = true;
 
+        public Stopwatch()
+        {
+            //this.endTime = new TimeSpan(1);
+            
+        }
+        
+        public void Start()
+        {
+            startTime = DateTime.Now;
+
+            //while (this.stopFlag)
+            //{
+            //    endTime = endTime.Add(TimeSpan.FromTicks(1));
+            //    //Console.WriteLine(this.endTime);
+            //}
+        } 
+
+        public TimeSpan Stop()
+        {
+            endTime = DateTime.Now;
+            TimeSpan interval = endTime - startTime;
+            return interval.Duration();
+        }
+        
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            Console.WriteLine(sw.Stop());
+
+            //Console.WriteLine(sw.Duration());
         }
     }
 }
