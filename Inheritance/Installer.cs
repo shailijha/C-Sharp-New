aedda739ed6fcbcfaba4ce33a075965542303436
shailijha <shaili_jha@yahoo.co.in>
2019-10-01 10:47:52 -0400

    installer class

---
+++ Inheritance/Installer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+
+    public class Installer
+    {
+        private readonly Logger _logger;
+
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Install()
+        {
+            _logger.Log("We are installing the application");
+        }
+    }
+}
