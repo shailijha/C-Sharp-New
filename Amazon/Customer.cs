9e895d9f1275bcb5004ebf6241ebc451a6bbee05
shailijha <shaili_jha@yahoo.co.in>
2019-10-01 10:41:51 -0400

    Amazon customer class

---
+++ Amazon/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public void Promote()
+        {
+            var calc = new RateCalculator();
+            var rating = calc.Calculate(this);
+
+        }
+
+        protected int CalculateRating(bool excludeOrders)
+        {
+            return 0;
+        }
+    }
+}
