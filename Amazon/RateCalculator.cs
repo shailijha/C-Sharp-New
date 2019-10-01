c5a473cac6f25e86b929948a4c987fc3d9a24ef6
shailijha <shaili_jha@yahoo.co.in>
2019-10-01 10:43:47 -0400

    RateCalculator class Amazon

---
+++ Amazon/RateCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon
+{
+    internal class RateCalculator
+    {
+        public int Calculate(Customer cust)
+        {
+            return 0;
+        }
+    }
+}
