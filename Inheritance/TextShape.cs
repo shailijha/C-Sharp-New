1bc1e9d76f21a0c3f0fcade3fbb46c71fbb7e458
shailijha <shaili_jha@yahoo.co.in>
2019-10-01 10:48:35 -0400

    text shape class

---
+++ Inheritance/TextShape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class TextShape: Shape
+    {
+        public int fontSize { get; set; }
+        public int fontName { get; set; }
+    }
+}
