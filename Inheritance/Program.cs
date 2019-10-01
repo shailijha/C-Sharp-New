07eba1177537e8923b66dcc52273911c79763119
shailijha <shaili_jha@yahoo.co.in>
2019-10-01 10:48:17 -0400

    inheritance main program

---
+++ Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿using Amazon;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var text = new Text();
+
+            //text.Width = 100;
+            //text.Copy();
+
+            //var dbMigrator = new DBMigrator(new Logger());
+
+            //var logger = new Logger();
+            //var installer = new Installer(logger);
+
+            //dbMigrator.Migrate();
+            //installer.Install();
+            //var customer = new Customer();
+            //Amazon.RateCalculator calc = new RateCalculator();
+            //TextShape txt = new TextShape();
+            //Shape shape = txt;
+
+            //txt.width = 200;
+            //shape.width = 100;
+
+            //Console.WriteLine(txt.width);
+
+            //StreamReader reader = new StreamReader
+        }
+    }
+}
