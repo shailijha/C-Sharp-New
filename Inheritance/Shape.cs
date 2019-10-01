8568306216a66126577b24c7e82c390813d98d48
shailijha <shaili_jha@yahoo.co.in>
2019-10-01 10:48:23 -0400

    shape class

---
+++ Inheritance/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Shape
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
