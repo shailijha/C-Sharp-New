dfccdca12ae2f1b9eb9dacfa1ecdc446002da4a0
shailijha <shaili_jha@yahoo.co.in>
2019-10-01 10:48:28 -0400

    text class

---
+++ Inheritance/Text.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Text : PresentationObject
+    {
+        public int FontSize { get; set; }
+        public int FontName { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine("We added a link to the " + url);
+        }
+    }
+}
